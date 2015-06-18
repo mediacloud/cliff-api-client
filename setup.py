10433983074c433d13e8315895364042655e3adc
Rahul Bhargava <rahulb@media.mit.edu>
2015-06-18 12:43:03 -0400

    prep for release to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,39 @@
 #! /usr/bin/env python
 
 from setuptools import setup
+import sys, re, logging
+
+version = ''
+with open('mediameter/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+if sys.argv[1]=="sdist":
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError) as e:
+        long_description = open('README.md').read()
+        logging.exception(e)
+    f = open('README.rst', 'w')
+    f.write(long_description)
+    f.close()
+
+readme_rst = ''
+with open('README.rst', 'r') as f:
+    readme_rst = f.read()
 
 setup(name='mediameter-cliff',
-  version='1.0',
-  description='MediaMeter CLIFF API Client Library',
-  author='Rahul Bhargava',
-  author_email='rahulb@media.mit.edu',
-  url='http://cliff.mediameter.org',
-  packages={'mediameter': 'mediameter',
-            'mediameter.test': 'mediameter/test'},
-  package_data={'mediameter.test':['fixtures/*.json']},
-  install_requires=['requests']
+    version=version,
+    description='MediaMeter CLIFF API Client Library',
+    long_description=readme_rst,
+    author='Rahul Bhargava',
+    author_email='rahulb@media.mit.edu',
+    url='http://cliff.mediameter.org',
+    packages={'mediameter'},
+    package_data={'':['LICENSE']},
+    include_package_data=True,
+    install_requires=['requests'],
+    license='MIT',
+    zip_safe=False,
+    extras_require={'db': ['pypubsub','pymongo']}
 )

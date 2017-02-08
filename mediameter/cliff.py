c9ecec13b2cd8943d18ef11083dddf3a103fb749
Rahul B <rahulbot@gmail.com>
2017-02-07 19:47:00 -0800

    support local text replacements

--- mediameter/cliff.py
+++ mediameter/cliff.py
@@ -2,36 +2,36 @@ import logging, json
 import requests
 
 class Cliff():
-    '''
-    Make requests to a CLIFF geo-parsing / NER server
-    '''
+    """Make requests to a CLIFF geo-parsing / NER server"""
 
     PARSE_TEXT_PATH = "/cliff-2.3.0/parse/text"
     PARSE_NLP_JSON_PATH = "/cliff-2.3.0/parse/json"
     PARSE_SENTENCES_PATH = "/cliff-2.3.0/parse/sentences"
     GEONAMES_LOOKUP_PATH = "/cliff-2.3.0/geonames"
 
-    JSON_PATH_TO_ABOUT_COUNTRIES = 'results.places.about.countries';
+    JSON_PATH_TO_ABOUT_COUNTRIES = 'results.places.about.countries'
 
     STATUS_OK = "ok"
 
-    def __init__(self,host,port):
+    def __init__(self, host, port, text_replacements={}):
         self._log = logging.getLogger(__name__)
         self._host = host
         self._port = int(port)
-        self._log.info("initialized CLIFF @ %s:%d", self._host,self._port)
+        self._replacements = text_replacements
+        self._log.info("initialized CLIFF @ %s:%d", self._host, self._port)
 
-    def parseText(self,text,demonyms=False):
-        return self._parseQuery(self.PARSE_TEXT_PATH, text, demonyms)
+    def parseText(self, text, demonyms=False):
+        cleaned_text = self._getReplacedText(text)
+        return self._parseQuery(self.PARSE_TEXT_PATH, cleaned_text , demonyms)
 
-    def parseSentences(self,json_object,demonyms=False):
+    def parseSentences(self, json_object, demonyms=False):
         return self._parseQuery(self.PARSE_SENTENCES_PATH, json.dumps(json_object), demonyms)
 
-    def parseNlpJson(self,json_object,demonyms=False):
+    def parseNlpJson(self, json_object, demonyms=False):
         return self._parseQuery(self.PARSE_NLP_JSON_PATH, json.dumps(json_object), demonyms)
 
-    def geonamesLookup(self,geonames_id):
-        return self._query(self.GEONAMES_LOOKUP_PATH, {'id':geonames_id})['results']
+    def geonamesLookup(self, geonames_id):
+        return self._query(self.GEONAMES_LOOKUP_PATH, {'id': geonames_id})['results']
 
     def _demonymsText(self, demonyms=False):
         return "true" if demonyms else "false"
@@ -39,14 +39,21 @@ class Cliff():
     def _urlTo(self, path):
         return self._host+":"+str(self._port)+path
 
-    def _parseQuery(self,path,text,demonyms=False):
-        payload = {'q':text,'replaceAllDemonyms':self._demonymsText(demonyms)}
+    def _getReplacedText(self, text):
+        replaced_text = text
+        for replace, find in self._replacements.iteritems():
+            replaced_text = text.replace(find, replace)
+        return replaced_text
+
+    def _parseQuery(self, path, text, demonyms=False):
+        payload = {'q': text, 'replaceAllDemonyms': self._demonymsText(demonyms)}
         self._log.debug("Querying %r (demonyms=%r)", path, demonyms)
         return self._query(path,payload)
     
-    def _query(self,path,args):
+    def _query(self, path, args):
         try:
-            r = requests.post( self._urlTo(path), data=args)
+            url = self._urlTo(path)
+            r = requests.post(url, data=args)
             self._log.debug('CLIFF says %r', r.content)
             return r.json()
         except requests.exceptions.RequestException as e:
--- mediameter/test/clifftest.py
+++ mediameter/test/clifftest.py
@@ -1,6 +1,9 @@
 import unittest, ConfigParser, json
 import mediameter.cliff
 
+GEONAME_LONDON_UK = 2643743
+GEONAME_LONDERRY_NH = 5088905
+
 class CliffTest(unittest.TestCase):
     '''
     A basic set of test cases to make sure the API can pull from the server correctly.
@@ -9,20 +12,35 @@ class CliffTest(unittest.TestCase):
     def setUp(self):
         self._config = ConfigParser.ConfigParser()
         self._config.read('settings.config')
-        self._cliff = mediameter.cliff.Cliff(self._config.get('cliff','host'),self._config.get('cliff','port'))
+        self._cliff = mediameter.cliff.Cliff(self._config.get('cliff', 'host'), self._config.get('cliff', 'port'))
 
     def testParseText(self):
         results = self._cliff.parseText("This is about Einstien at the IIT in New Delhi.")['results']
-        self.assertEqual(len(results['organizations']),1)
-        self.assertEqual(len(results['places']['mentions']),1)
-        self.assertEqual(results['places']['mentions'][0]['id'],1261481)
-        self.assertEqual(len(results['people']),1)
+        self.assertEqual(len(results['organizations']), 1)
+        self.assertEqual(len(results['places']['mentions']), 1)
+        self.assertEqual(results['places']['mentions'][0]['id'], 1261481)
+        self.assertEqual(len(results['people']), 1)
 
     def testGeonamesLookup(self):
         results = self._cliff.geonamesLookup(4943351)
-        self.assertEqual(results['id'],4943351)
-        self.assertEqual(results['lon'],-71.09172)
-        self.assertEqual(results['lat'],42.35954)
-        self.assertEqual(results['name'],"Massachusetts Institute of Technology")
-        self.assertEqual(results['parent']['name'],"Middlesex County")
-        self.assertEqual(results['parent']['parent']['name'],"Massachusetts")
+        self.assertEqual(results['id'], 4943351)
+        self.assertEqual(results['lon'], -71.09172)
+        self.assertEqual(results['lat'], 42.35954)
+        self.assertEqual(results['name'], "Massachusetts Institute of Technology")
+        self.assertEqual(results['parent']['name'], "Middlesex County")
+        self.assertEqual(results['parent']['parent']['name'], "Massachusetts")
+
+    def testLocalReplacements(self):
+        replacements = {
+            'Londonderry': 'London',
+        }
+        # make sure non-replaced fetches the city in the UK
+        results = self._cliff.parseText("This is about London.")['results']
+        mention = results['places']['mentions'][0]
+        self.assertEqual(GEONAME_LONDON_UK, mention['id'])
+        # now see if it gets the city with replacements
+        replacing_cliff = mediameter.cliff.Cliff(self._config.get('cliff', 'host'),
+                                                 self._config.get('cliff', 'port'), replacements)
+        results = replacing_cliff.parseText("This is about London.")['results']
+        replaced_mention = results['places']['mentions'][0]
+        self.assertEqual(GEONAME_LONDERRY_NH, replaced_mention['id'])
